8f2b8fed418a36e427dc003d0d03d8b5beef7ebf
Oliver <olivercreativeworks@gmail.com>
2023-06-08 07:19:41 -0400

    Add method to organize by table stubs

--- UpdateApplication/Tracker.test.ts
+++ UpdateApplication/Tracker.test.ts
@@ -22,4 +22,27 @@ describe("Testing Tracker", () => {
             expect(Tracker.headersAsKeys([["headerA", "headerB"], [2, "happy"]])).toEqual({"headerA":[2], "headerB":["happy"]})
         })
     })
+
+    describe("Return array of rows with values mapped to the headers", () => {
+        test("One Column", () => {
+            expect(Tracker.getStubValues([["headerA"], ["value1"]])).toEqual([{"headerA": "value1"}])
+        })
+        test("Multiple Columns", () => {
+            expect(Tracker.getStubValues([["headerA", "headerB"], ["value1", "value2"]])).toEqual([{"headerA": "value1", "headerB":"value2"}])
+        })
+        test("Multiple Values In A Column", () =>{
+            expect(Tracker.getStubValues([["headerA"], ["value1"], ["value1A"]])).toEqual([{"headerA":"value1"}, {"headerA":"value1A"}])
+        })
+        test("Empty string", () => {
+            expect(Tracker.getStubValues([["headerA"], [""], ["value1A"]])).toEqual([{"headerA":""}, {"headerA":"value1A"}])
+            expect(Tracker.getStubValues([["headerA"], ["value1"], [""]])).toEqual([{"headerA":"value1"}, {"headerA":""}])
+            expect(Tracker.getStubValues([["headerA"], [""], [""]])).toEqual([{"headerA":""},{"headerA":""}])
+        })
+        test("Numbers", () => {
+            expect(Tracker.getStubValues([["headerA"], [2]])).toEqual([{"headerA":2}])
+        })
+        test("Strings and Numbers", () => {
+            expect(Tracker.getStubValues([["headerA", "headerB"], [2, "happy"]])).toEqual([{"headerA":2, "headerB":"happy"}])
+        })
+    })
 })
--- UpdateApplication/Tracker.ts
+++ UpdateApplication/Tracker.ts
@@ -4,5 +4,10 @@ export class Tracker{
         const body = x.slice(1) as Array<Array<A>>
         return Object.fromEntries(headers.map((header, index) =>  [header, body.map(row => row[index])]))
     }
+    static getStubValues<A>(x:[Array<string>, ...Array<Array<A>>]): Array<Record<string, A>>{
+        const headers = x[0]
+        const rows = x.slice(1) as Array<Array<A>>
+        return rows.map(row => Object.fromEntries(row.map((element, index) => [headers[index], element])))
+    }
 }
     
