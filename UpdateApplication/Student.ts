7aada6b812203c49deafcf05455c2dad9036ffb0
Oliver <olivercreativeworks@gmail.com>
2023-06-12 18:29:22 -0400

    Create student type

---
+++ UpdateApplication/Student.ts
@@ -0,0 +1,23 @@
+export type Student ={
+    firstName:string
+    lastName:string
+    fullName:string
+    assessmentDate: Date
+    phone:string
+    email:string
+    employer:string
+    job:string
+    inConstruction:boolean
+    hasOsha10Card:boolean
+    address:string
+    borough:string
+    councilDistrict:number
+    isNychaResident:boolean
+    development:string
+    hasValidId:boolean
+    assessment:string
+    id:string
+    certificate:string
+    osha30Card:string
+}
+
