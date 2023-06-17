38e579be10b881a69c7109eb12552bf2763190a6
Oliver <olivercreativeworks@gmail.com>
2023-06-17 06:17:50 -0400

    Create functions to update assessments in grid

---
+++ UpdateApplication/Main.ts
@@ -0,0 +1,45 @@
+import { Assessment } from "./Assessment"
+import { MyGlobals } from "./GLOBALS"
+
+function main(){
+    updateAssessments()
+}
+
+function updateAssessments(){
+    const studentData = MyGlobals.getStudentData()
+    
+    const updatedData = studentData
+        .updateCol("assessment", createAssessment, textIsEmptyString)
+        .updateCol("assessment", fillAssessment, hasUrl)
+    
+    return updatedData
+}
+
+function mapObjectValues<A extends string, B, C>(obj: Record<A, B>, fn:(arg:B) => C):Record<A, C>{
+    return Object.fromEntries( Object.entries(obj).map(([k,v]) => [k, fn(v as B)]) ) as Record<A, C>
+}
+
+function fillAssessment(richTextStudent:MyGlobals.RichTextStudent):MyGlobals.RichText{
+    const assessment = DocumentApp.openByUrl(richTextStudent.assessment.url as string)
+    const student = mapObjectValues( richTextStudent, (richText) => richText.text)
+    Assessment.fillIn(student, assessment)
+    return richTextStudent.assessment
+}
+
+function hasUrl(richText:MyGlobals.RichText): richText is {text:string, url:string}{
+    return typeof richText.url === "string"
+}
+
+function textIsEmptyString(richText:MyGlobals.RichText):richText is {text:""}{
+    return richText.text === ""
+}
+
+function createAssessment(richTextStudent:MyGlobals.RichTextStudent):MyGlobals.RichText{
+    const name = richTextStudent.fullName.text
+    const url = Assessment.retrieve(name).orElse(Assessment.create(name)).getUrl()
+    return createRichText("LINK", url)
+}
+
+function createRichText(text:string, url?:string):MyGlobals.RichText{
+    return {text, url}
+}
