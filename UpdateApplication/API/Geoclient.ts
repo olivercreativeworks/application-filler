a5e18d48d7b82806cbc3b9ad1464079b2cc3150c
Oliver <olivercreativeworks@gmail.com>
2023-06-23 07:08:49 -0400

    Move Api calls into separate folder

---
+++ UpdateApplication/API/Geoclient.ts
@@ -0,0 +1,39 @@
+import { Credentials } from "../Credentials"
+
+export namespace Geoclient{
+    export interface RawData{
+        buildingIdentificationNumber:number // an id number assigned to each building in nyc
+        houseNumber:number // house number (i.e. 425 or 4-25)
+        houseNumberIn:number // house number as integer (i.e. 425)
+        firstStreetNameNormalized:string // full street name (i.e. Astoria Boulevard)
+        streetName1In:string // abbreviated street (i.e. Astoria Blvd)
+        firstBoroughName:string
+        zipCode:number
+        cityCouncilDistrict:number
+    }
+
+    export function getRawGeoclientData(address:string):RawData | undefined{
+        try{
+            const appId = Credentials.getGeoclientAPIAppId()
+            const url = `https://api.nyc.gov/geo/geoclient/v1/search.json?input=${address}&appId=${appId} HTTP/1.1`
+    
+            const SECRET_KEY = Credentials.getGeoclientSecretKey()
+            const options:GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+                method:"get",
+                headers:{
+                    "Ocp-Apim-Subscription-Key": SECRET_KEY
+                }
+            }
+    
+            const response = UrlFetchApp.fetch(url, options)
+            const parsedResponse = JSON.parse(response.getContentText())
+            const results = parsedResponse?.results as [{response: RawData} | undefined]
+            return results[0]?.response
+        }
+        catch(err){
+            Logger.log(address)
+            Logger.log(err)
+            return undefined
+        }
+    }
+}
---
+++ UpdateApplication/API/NycOpenData.ts
@@ -0,0 +1,35 @@
+import { Credentials } from "../Credentials"
+
+export namespace NycOpenData{
+    export interface RawNychaResidentialData{
+        zip_code: number
+        address: string
+        development:string
+        city:string
+        city_council_district:number
+        state:string
+        bin:number
+        street: string
+        borough:string
+    }
+    export function getRawNychaResidentialData(bin:number):RawNychaResidentialData | undefined{
+        try{
+            const APP_TOKEN = Credentials.getNycOpenDataAppToken()
+            const url = `https://data.cityofnewyork.us/resource/3ub5-4ph8.json?bin=${bin}&$$app_token=${APP_TOKEN}`
+    
+            const options:GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+                method:"get"
+            }
+    
+            const jsonResponse = UrlFetchApp.fetch(url, options)
+            const parsedResponse = JSON.parse(jsonResponse.getContentText()) as [RawNychaResidentialData | undefined]
+            return parsedResponse[0]
+        }
+        catch(err){
+            Logger.log(bin)
+            Logger.log(err)
+            return undefined
+        }
+    }
+
+}
