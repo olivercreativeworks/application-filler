c94714f4509b9914f3f6d94068f5ffbcc8058ec7
Oliver <olivercreativeworks@gmail.com>
2023-06-23 06:30:38 -0400

    Remove unnecessary functions and imports

--- UpdateApplication/Main.ts
+++ UpdateApplication/Main.ts
@@ -1,20 +1,12 @@
-import { Address } from "./Address"
 import { AddressWriter } from "./Address.writer"
-import { Assessment } from "./Assessment"
 import { AssessmentWriter } from "./Assessment.writer"
-import { DynamicGrid } from "./DynamicGrid"
 import { MyGlobals } from "./GLOBALS"
-import { Maybe, Unwrap } from "./Maybe"
-import { Utility } from "./Utility"
+import { Maybe } from "./Maybe"
+
 
 function main(){
-    updateAssessments()
-}
-
-function updateAssessments(){
     const studentData = MyGlobals.getStudentData()
-    const updatedData = Maybe.of(studentData)
+    Maybe.of(studentData)
         .map(AddressWriter.updateStudentAddressFields)
         .map(AssessmentWriter.updateAssessments)
-    return updatedData
 }
