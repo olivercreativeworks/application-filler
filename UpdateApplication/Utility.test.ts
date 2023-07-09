df67e8dcf863f536fbadcd868e5ca13b77447977
Oliver <olivercreativeworks@gmail.com>
2023-07-09 10:04:17 -0400

    Add methods to test equality

--- UpdateApplication/Utility.test.ts
+++ UpdateApplication/Utility.test.ts
@@ -229,3 +229,230 @@ describe("Memoize", () => {
         })
     })
 })
+
+describe("Is Null", () => {
+    const isNull = Utility.isNull
+
+    test("Should return true", () => { 
+        expect(isNull(null)).toEqual(true)
+    })
+    test("Should return false", () => {
+        expect(isNull([null])).toEqual(false)
+        expect(isNull([])).toEqual(false)
+        expect(isNull({})).toEqual(false)
+        expect(isNull(undefined)).toEqual(false)
+        expect(isNull("")).toEqual(false)
+        expect(isNull(0)).toEqual(false)
+        expect(isNull(false)).toEqual(false)
+    })
+})
+
+describe("Is Undefined", () => {
+    const isUndefined = Utility.isUndefined
+
+    test("Should return true", () => {  
+        expect(isUndefined(undefined)).toEqual(true)
+    })
+    test("Should return false", () => {  
+        expect(isUndefined([undefined])).toEqual(false)
+        expect(isUndefined([])).toEqual(false)
+        expect(isUndefined({})).toEqual(false)
+        expect(isUndefined(null)).toEqual(false)
+        expect(isUndefined("")).toEqual(false)
+        expect(isUndefined(0)).toEqual(false)
+        expect(isUndefined(false)).toEqual(false)
+    })
+})
+
+describe("Testing Equality" , () => {
+    const areEqual = Utility.areEqual
+
+    describe("Primatives", () => {
+        describe("Should return true", () => {
+            test("Boolean", () => {
+                expect(areEqual(true, true)).toEqual(true)
+                expect(areEqual(false, false)).toEqual(true)
+            })
+            test("Number", () => {
+                expect(areEqual(1, 1)).toEqual(true)
+                expect(areEqual(1+1, 2)).toEqual(true)
+            })
+            test("String", () => {
+                expect(areEqual("a", "a")).toEqual(true)
+            })
+        })
+        describe("Should return false" , () => {
+            test("Boolean", () => {
+                expect(areEqual(true, false)).toEqual(false)
+                expect(areEqual(false, null)).toEqual(false)
+                expect(areEqual(false, 0)).toEqual(false)
+                expect(areEqual(false, undefined)).toEqual(false)
+                expect(areEqual(false, [])).toEqual(false)
+                expect(areEqual(false, {})).toEqual(false)
+                expect(areEqual(false, "")).toEqual(false)
+            })
+            test("Number", () => {
+                expect(areEqual(1, false)).toEqual(false)
+                expect(areEqual(1+1, false)).toEqual(false)
+            })
+            test("String", () => {
+                expect(areEqual("a", false)).toEqual(false)
+            })
+        })
+    })
+    describe("Null", () => {
+        test("Should return true", () => {
+            expect(areEqual(null, null)).toEqual(true)
+        })
+
+        test("Should return false", () => {
+            expect(areEqual(null, [null])).toEqual(false)
+            expect(areEqual(null, [])).toEqual(false)
+            expect(areEqual(null, {})).toEqual(false)
+            expect(areEqual(null, undefined)).toEqual(false)
+            expect(areEqual(null, "")).toEqual(false)
+            expect(areEqual(null, 0)).toEqual(false)
+            expect(areEqual(null, false)).toEqual(false)
+        })
+    })
+    describe("Undefined", () => {
+        test("Should return true", () => {
+            expect(areEqual(undefined, undefined)).toEqual(true)
+        })
+        test("Should return false", () => {
+            expect(areEqual(undefined, [undefined])).toEqual(false)
+            expect(areEqual(undefined, [])).toEqual(false)
+            expect(areEqual(undefined, {})).toEqual(false)
+            expect(areEqual(undefined, null)).toEqual(false)
+            expect(areEqual(undefined, "")).toEqual(false)
+            expect(areEqual(undefined, 0)).toEqual(false)
+            expect(areEqual(undefined, false)).toEqual(false)
+        })
+    })
+    describe("Array", () => {
+        describe("Should return true", () => {
+            test("Empty Array", () => {
+                expect(areEqual(Array.of(), Array.of())).toEqual(true)
+            })
+            test("One Dimensional Array", () => {
+                expect(areEqual(Array.of(true), Array.of(true))).toEqual(true)
+            })
+            test("Multi-Dimensional Array", () =>{
+                expect(areEqual(Array.of(Array.of(true)), Array.of(Array.of(true)))).toEqual(true)
+            })
+            test("One element", () => {
+                expect(areEqual(Array.of(1), Array.of(1))).toEqual(true)
+            })
+            test("Multiple elements", () => {
+                expect(areEqual(Array.of(1, 2), Array.of(1, 2))).toEqual(true)
+            })
+            test("Object as element", () => {
+                expect(areEqual(Array.of(Object.fromEntries([["a",1]])), Array.of(Object.fromEntries([["a",1]])))).toEqual(true)
+            })
+            test("Object as element / same keys, different insertion order", () => {
+                expect(areEqual(Array.of(Object.fromEntries([["a",1], ["b", 2]])), Array.of(Object.fromEntries([["b",2], ["a", 1]])))).toEqual(true)
+            })
+            test("Multi-dimensional array / Object as element / same keys, different insertion order", () => {
+                expect(areEqual([[{b:2, a:1}]], [[{a:1, b:2}]])).toEqual(true)
+            })
+            test("Multi-dimensional array / Nested object as element / same keys, different insertion order", () => {
+                expect(areEqual([[{b:{c:3, d:4, f:5}, a:1}]], [[{a:1, b:{d:4, c:3, f:5}}]])).toEqual(true)
+            })
+            test("Contains null element", () => {
+                expect(areEqual([null], [null])).toEqual(true)
+            })
+            test("Contains undefined element", () => {
+                expect(areEqual([undefined], [undefined])).toEqual(true)
+            })
+            test("Contains a mix of types", () => {
+                expect(areEqual([undefined, "", [null], {a:3}], [undefined, "", [null], {a:3}])).toEqual(true)
+
+            })
+        })
+        describe("Should return false", () => {
+            test("Arrays have different elements", () => {
+                expect(areEqual(Array.of(1), Array.of(2))).toEqual(false)
+            })
+            test("Arrays have same elements, but in different order", () => {
+                expect(areEqual(Array.of(1,2), Array.of(2,1))).toEqual(false)
+            })
+            test("Multi-dimensional arrays have same elements, but in different order", () => {
+                expect(areEqual(Array.of(Array.of(1,2)), Array.of(Array.of(2,1)))).toEqual(false)
+                expect(areEqual(Array.of(Array.of(1), Array.of(2)), Array.of(Array.of(2), Array.of(1)))).toEqual(false)
+            })
+            test("Empty array and single null value", () => {
+                expect(areEqual([null], [])).toEqual(false)
+            })
+            test("Empty array and single undefined value", () => {
+                // I'm choosing to treat these as different values.
+                // When you use map on an array that has a single undefined element, the map function will run
+                // If you map on an empty array, the map function will not run
+                expect(areEqual([undefined], [])).toEqual(false)
+            })
+        })
+    })
+    describe("Objects", () => {
+        describe("Should return true", () => {
+            test("Empty object", () => {
+                expect(areEqual(Object.fromEntries([[,,]]), Object.fromEntries([[,,]]))).toEqual(true)
+            })
+            test("One key", () => {
+                expect(areEqual(Object.fromEntries([["a", 1]]), Object.fromEntries([["a", 1]]))).toEqual(true)
+            })
+            test("Multiple keys / same insertion order", () => {
+                expect(areEqual(Object.fromEntries([["a", 1], ["b", 2]]), Object.fromEntries([["a", 1], ["b", 2]]))).toEqual(true)
+            })
+            test("Multiple keys / different insertion order", () => {
+                expect(areEqual(Object.fromEntries([["a", 1], ["b", 2]]), Object.fromEntries([["b", 2], ["a", 1]]))).toEqual(true)
+            })
+            test("One key with an array as a value", () => {
+                expect(areEqual(Object.fromEntries([["a", Array.of(1)]]), Object.fromEntries([["a", Array.of(1)]]))).toEqual(true)
+            })
+            test("Nested object with same keys and values", () => {
+                expect(areEqual(Object.fromEntries([["a", Object.fromEntries([["a1", 1]])]]), Object.fromEntries([["a", Object.fromEntries([["a1", 1]])]]))).toEqual(true)
+            })
+            test("Same keys, same values / values are undefined", () => {
+                expect(areEqual({a:undefined}, {a:undefined})).toEqual(true)
+            })
+            test("Same keys, same values / values are null", () => {
+                expect(areEqual({a:null}, {a:null})).toEqual(true)
+            })
+            test("Same keys, but one value is null, the other is undefined", () => {
+                // I'm choosing to treat these as the same value
+                // If you try to access a property on an object with an undefined value, the output value will be null
+                // This means accessing property "a" on {a:null} and {a:undefined} will produce the same value (null)
+                expect(areEqual({a:null}, {a:undefined})).toEqual(true)
+            })
+        })
+        describe("Should return false", () => {
+            test("Different keys and values", () => {
+                expect(areEqual(Object.fromEntries([["a",1]]), Object.fromEntries([["b",2]]))).toEqual(false)
+            })
+            test("Same keys different values", () => {
+                expect(areEqual(Object.fromEntries([["a",1]]), Object.fromEntries([["a",2]]))).toEqual(false)
+            })
+            test("Different keys same values", () => {
+                expect(areEqual(Object.fromEntries([["a",1]]), Object.fromEntries([["b",1]]))).toEqual(false)
+            })
+            test("Same keys different array values", () => {
+                expect(areEqual(Object.fromEntries([["a", Array.of(1,2)]]), Object.fromEntries([["a", Array.of(2,1)]]))).toEqual(false)
+            })
+            test("Nested object with different keys and values", () => {
+                expect(areEqual(Object.fromEntries([["a", Object.fromEntries([["a1", 1]])]]), Object.fromEntries([["a", Object.fromEntries([["b1", 2]])]]))).toEqual(false)
+            })
+            test("Nested object with same keys but different values", () => {
+                expect(areEqual(Object.fromEntries([["a", Object.fromEntries([["a1", 1]])]]), Object.fromEntries([["a", Object.fromEntries([["a1", 2]])]]))).toEqual(false)
+            })
+            test("Nested object with different keys but same values", () => {
+                expect(areEqual(Object.fromEntries([["a", Object.fromEntries([["a1", 1]])]]), Object.fromEntries([["a", Object.fromEntries([["b1", 1]])]]))).toEqual(false)
+            })
+            test("Object where all keys have a null value vs empty object", () => {
+                expect(areEqual({a:null}, {})).toEqual(false)
+            })
+            test("Object where all keys have a undefined value vs empty object", () => {
+                expect(areEqual({a:undefined}, {})).toEqual(false)
+            })
+            
+        })
+    })
+})
--- UpdateApplication/Utility.ts
+++ UpdateApplication/Utility.ts
@@ -1,10 +1,10 @@
-export interface MyIterator<A>{
+export interface MyGoogleIterator<A>{
     hasNext():boolean
     next(): A
 }
 
 export namespace Utility{
-    export function loop<A, B>(fn:(x:A, y?:B) => B, iterator:MyIterator<A>, maxIterations:number = Infinity):B | undefined{
+    export function loop<A, B>(fn:(x:A, y?:B) => B, iterator:MyGoogleIterator<A>, maxIterations:number = Infinity):B | undefined{
         let result = undefined
         for(let i = 0; i<maxIterations && iterator.hasNext(); i++){
            result = fn(iterator.next(), result)
@@ -12,7 +12,7 @@ export namespace Utility{
         return result
     }
     
-    export function collect<A, B, C>(iterator:MyIterator<A>, fn:(x:A, y:Map<B,C>) => Map<B,C>, maxIterations:number = Infinity):Map<B,C>{
+    export function collect<A, B, C>(iterator:MyGoogleIterator<A>, fn:(x:A, y:Map<B,C>) => Map<B,C>, maxIterations:number = Infinity):Map<B,C>{
         let map = new Map() as Map<B,C>
         for(let i = 0; i<maxIterations && iterator.hasNext(); i++){
             const iterable = iterator.next()
@@ -46,4 +46,50 @@ export namespace Utility{
             return storedResult
         }
     }
+    
+    export function areEqual(x:unknown, y:unknown):boolean{
+        if (x === null || y === null) 
+            return bothValuesAreNull(x, y)      
+        else if (x === undefined || y === undefined) 
+            return bothValuesAreUndefined(x, y)    
+        else if (Array.isArray(x) && Array.isArray(y)) 
+            return arrayElementsToJson(x) === arrayElementsToJson(y)
+        else if (isObject(x) && isObject(y)) 
+            return objectToJson(x) === objectToJson(y)
+        return JSON.stringify(x) === JSON.stringify(y)
+   
+        function bothValuesAreNull(x:unknown, y:unknown):boolean{
+            return (x === null) && (y === null)
+        }
+        function bothValuesAreUndefined(x:unknown, y:unknown):boolean{
+            return (x === undefined) && (y === undefined)
+        }
+        
+        function objectToJson(x:object):string{
+            if(x === null || x === undefined){ return x}
+            return JSON.stringify(Object.entries(x).sort().map(([key,val]) => [key, jsonify(val)]))
+        }
+    
+        function arrayElementsToJson(x:Array<any>):string{
+            return JSON.stringify(x.map(jsonify))
+        }
+        
+        function jsonify(x:unknown):string{
+            if(Array.isArray(x)){ return arrayElementsToJson(x) }
+            else if (isObject(x)){ return objectToJson(x) }
+            return JSON.stringify(x)
+        }
+    
+        function isObject(x:unknown): x is object{
+            return typeof x === "object"
+        }
+    }
+
+    export function isNull(x:unknown): x is null{
+        return x === null
+    }
+    export function isUndefined(x:unknown): x is undefined{
+        return x === undefined
+    }
+
 }
