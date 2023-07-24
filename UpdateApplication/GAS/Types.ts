fe111a9f4d8bb5be191c17969ec3f31e519cc3f8
Oliver <olivercreativeworks@gmail.com>
2023-07-24 07:08:38 -0400

    Make type alias file for google apps script types

---
+++ UpdateApplication/GAS/Types.ts
@@ -0,0 +1,5 @@
+export type GoogleDocument = GoogleAppsScript.Document.Document
+export type GoogleDocumentNamedRange = GoogleAppsScript.Document.NamedRange
+export type GoogleFile = GoogleAppsScript.Drive.File
+export type GoogleInlineImage = GoogleAppsScript.Document.InlineImage
+export type GoogleParagraph = GoogleAppsScript.Document.Paragraph
